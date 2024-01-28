c4ede57e15b4cb1efd34ae2c0a2c453adfed5b21
pardo-maria <maria.pardo2408@gmail.com>
2024-01-28 16:17:34 +0100

    Modified flatten and evaluate methods to take datasets as inputs

--- scripts/knnModel.py
+++ scripts/knnModel.py
@@ -2,7 +2,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report, ConfusionMatrixDisplay
 from imageLoader import ImageLoader
 import matplotlib.pyplot as plt
-
+import numpy as np
 
 class KNNModel(ImageLoader):
 
@@ -13,29 +13,25 @@ class KNNModel(ImageLoader):
         self.k = k
         self.model = KNeighborsClassifier(n_neighbors=k)
 
-    def flatten(self):
-        train_images = self.train_data[0]
-        test_images = self.test_data[0]
+    @staticmethod
+    def flatten(tensor_dataset):
+        images, labels = tuple(zip(*tensor_dataset.unbatch()))
+        images = np.array(images)
+        labels = np.array(labels)
+        nsamples, nx, ny, nrgb = images.shape
+        flat = images.reshape(nsamples, nx*ny*nrgb)
+        return flat, labels
 
-        train_nsamples, train_nx, train_ny, train_nrgb = train_images.shape
-        test_nsamples, test_nx, test_ny, test_nrgb = test_images.shape
-
-        train_flat = train_images.reshape(train_nsamples, train_nx*train_ny*train_nrgb)
-        test_flat = test_images.reshape(test_nsamples, test_nx*test_ny*test_nrgb)
-        return train_flat, test_flat
-
-    def evaluate(self):
-        x_train = self.flatten()[0]
-        y_train = self.train_data[1]
-        x_test = self.flatten()[1]
-        y_test = self.test_data[1]
+    def evaluate(self, train, test):
+        x_train, y_train = self.flatten(train)
+        x_test, y_test = self.flatten(test)
 
         self.model.fit(x_train, y_train)
         y_pred = self.model.predict(x_test)
         accuracy = accuracy_score(y_test, y_pred)
 
         matrix = confusion_matrix(y_test, y_pred)
-        disp = ConfusionMatrixDisplay(confusion_matrix=matrix, display_labels=[0, 1, 2, 3, 4, 5])
+        disp = ConfusionMatrixDisplay(confusion_matrix=matrix, display_labels=['buildings', 'forest', 'glacier', 'mountain', 'sea', 'street'])
 
         print(classification_report(y_pred, y_test))
         disp.plot()
