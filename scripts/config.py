7774e856187f45d5e396876dc2339f0eed1018f3
jwegrzynek <kuba.lysy.weg@gmail.com>
2024-01-28 13:30:16 +0100

    Added class with image size for first loading

--- scripts/config.py
+++ scripts/config.py
@@ -14,9 +14,16 @@ class Labels:
     }
 
 
+# Default params for image augmentation
 class AugmentImageParams:
     rotate = 0.15
     zoom = (.2, .1)
     flip = "horizontal"
     contrast = 0.3
     brightness = 0.001
+
+
+# Default size of loaded image (base images are 150x150 pixels)
+class ImageLoadSize:
+    height = 150
+    width = 150
