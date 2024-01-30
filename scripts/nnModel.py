09bde624dd66ef92ddf09f80abf9ce4ea7078bf3
natka178 <nszczepkowska@interia.pl>
2024-01-30 18:12:38 +0100

    Corrected docstrings in class NNModel

--- scripts/nnModel.py
+++ scripts/nnModel.py
@@ -6,14 +6,12 @@ class NNModel(BaseNNModel):
     """
         A neural network model for image classification, built on top of the BaseNNModel.
 
-        Args:
-        - dense_layers (tuple): Tuple specifying the number of neurons in each dense layer. Default is (128, 64, 6).
-        - activations (tuple): Tuple specifying the activation function for each dense layer. Default is ('relu', 'relu', 'softmax').
-        - input_shape (tuple): Tuple specifying the input shape of the images. Default is (150, 150, 3).
+        All parameters can be set in config.py.
 
         Attributes:
-        - dense_layers (tuple): Tuple specifying the number of neurons in each dense layer.
-        - activations (tuple): Tuple specifying the activation function for each dense layer.
+        - dense_layers (tuple): Tuple specifying the number of neurons in each dense layer. Default is (128, 64, 6).
+        - activations (tuple): Tuple specifying the activation function for each dense layer. Default is ('relu', 'relu', 'softmax').
+        - input_shape (tuple): Tuple specifying the input shape of the images. Default is (150, 150, 3)
         - model: The Sequential model defining the neural network architecture.
 
         Methods:
@@ -25,8 +23,9 @@ class NNModel(BaseNNModel):
         super().__init__()
         self.dense_layers = dense_layers
         self.activations = activations
+        self.input_shape = input_shape
         self.model = tf.keras.models.Sequential([
-            tf.keras.layers.Flatten(input_shape=input_shape),
+            tf.keras.layers.Flatten(input_shape=self.input_shape),
             *[tf.keras.layers.Dense(
                 dense_layer, activation=activation) for dense_layer, activation in
                 zip(self.dense_layers, self.activations)]
