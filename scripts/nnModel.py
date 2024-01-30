7fc720219efad87d910c9d54e401f1a68f4ddf42
jwegrzynek <kuba.lysy.weg@gmail.com>
2024-01-30 20:13:07 +0100

    Removed parameter

--- scripts/nnModel.py
+++ scripts/nnModel.py
@@ -11,7 +11,6 @@ class NNModel(BaseNNModel):
         Attributes:
         - dense_layers (tuple): Tuple specifying the number of neurons in each dense layer. Default is (128, 64, 6).
         - activations (tuple): Tuple specifying the activation function for each dense layer. Default is ('relu', 'relu', 'softmax').
-        - input_shape (tuple): Tuple specifying the input shape of the images. Default is (150, 150, 3)
         - model: The Sequential model defining the neural network architecture.
 
         Methods:
@@ -19,13 +18,12 @@ class NNModel(BaseNNModel):
         - evaluate_model(test_data): Evaluates the neural network model on the provided test data.
         - save_model(model_path): Saves the trained model to the specified file path.
     """
-    def __init__(self, dense_layers=(128, 64, 6), activations=('relu', 'relu', 'softmax'), input_shape=(150, 150, 3)):
+    def __init__(self, dense_layers=(128, 64, 6), activations=('relu', 'relu', 'softmax')):
         super().__init__()
         self.dense_layers = dense_layers
         self.activations = activations
-        self.input_shape = input_shape
         self.model = tf.keras.models.Sequential([
-            tf.keras.layers.Flatten(input_shape=self.input_shape),
+            tf.keras.layers.Flatten(),
             *[tf.keras.layers.Dense(
                 dense_layer, activation=activation) for dense_layer, activation in
                 zip(self.dense_layers, self.activations)]
